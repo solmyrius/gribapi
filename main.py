26468c3b3cc24a2b3afbdca9e15ecac3f77b8bda
solmyrius <serge.kharkov@gmail.com>
2022-12-22 18:29:53 +0200

    Allow use params from command line

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 import os
+import sys
 
 from dotenv import load_dotenv
 from gribreader import GRIBReader
 
 load_dotenv()
 
-reader = GRIBReader(os.getenv("SOURCE_GRIB2_FILE"))
-reader.run(os.getenv("SOURCE_CSV_FILE"), os.getenv("DESTINATION_CSV_FILE"))
+
+if __name__ == "__main__":
+    src_grib = None
+    src_csv = None
+    dst_csv = os.getenv("DESTINATION_CSV_FILE", 'result.csv')
+
+    if len(sys.argv) >= 3:
+        src_grib = sys.argv[1]
+        src_csv = sys.argv[2]
+
+    if src_grib is None:
+        src_grib = os.getenv("SOURCE_GRIB2_FILE")
+
+    if src_csv is None:
+        src_csv = os.getenv("SOURCE_CSV_FILE")
+
+    reader = GRIBReader(src_grib)
+    reader.run(src_csv, dst_csv)
